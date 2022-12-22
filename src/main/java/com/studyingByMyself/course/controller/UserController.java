427c7e2b6a43f7b35932af0a20a45c9820d66dbd
Eduardo Greff <eduardogreff0gmail.com>
2022-12-22 17:19:46 -0300

    Service layer and component registration 2

--- src/main/java/com/studyingByMyself/course/controller/UserController.java
+++ src/main/java/com/studyingByMyself/course/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.studyingByMyself.course.controller;
 
 import com.studyingByMyself.course.entities.User;
-import com.studyingByMyself.course.repository.UserRepository;
 import com.studyingByMyself.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
